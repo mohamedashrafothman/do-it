f0287b937636af81265260aa023767625caa48fe
Mohamed Ashraf <mashraf@road9media.com>
2023-03-21 04:12:04 +0200

    build: fixes in commitlint config file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /**
  * Following The Conventional Commits specification is a lightweight convention on top of commit messages.
  * It provides an easy set of rules for creating an explicit commit history;
